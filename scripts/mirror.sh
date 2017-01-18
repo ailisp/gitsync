7e3368c73072f58fe39b5ea4ffd2728b3be66666
Konstantin <konstantin.k.ed@gmail.com>
2017-01-18 13:15:12 +0000

    logging

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -3,9 +3,15 @@
 DIR=git_workdir
 mkdir $DIR
 cd $DIR
-git clone --mirror GIT_SRC_ADDRESS
+
+echo Cloning $GIT_SRC_ADDRESS
+git clone --mirror $GIT_SRC_ADDRESS
+
 cd parity.git
-git push --mirror GIT_TRG_ADDRESS
+
+echo Pusing to $GIT_TRG_ADDRESS
+git push --mirror $GIT_TRG_ADDRESS
+
 cd ../../
 rm -rf $DIR
 
