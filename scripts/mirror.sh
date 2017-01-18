08412ca694810e5a695413e8f39f2a9bfb8a53d4
Konstantin <konstantin.k.ed@gmail.com>
2017-01-18 13:13:59 +0000

    removed hardcoded git urls

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -3,9 +3,9 @@
 DIR=git_workdir
 mkdir $DIR
 cd $DIR
-git clone --mirror git@github.com:jesuscript/parity.git
+git clone --mirror GIT_SRC_ADDRESS
 cd parity.git
-git push --mirror git@gitlab.ethcore.io:parity/parity.git
+git push --mirror GIT_TRG_ADDRESS
 cd ../../
 rm -rf $DIR
 
