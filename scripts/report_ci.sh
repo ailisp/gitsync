95ca69094999d52b16c1ed939cad6d1a6f89014e
Bo Yao <bo@nearprotocol.com>
2019-09-27 15:05:39 -0700

    add report to ci

---
+++ scripts/report_ci.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DEFAULT_POLL_TIMEOUT=10
+POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
+
+sleep $POLL_TIMEOUT
+
+pipeline_id=$(curl --header "PRIVATE-TOKEN: $GITLAB_PASSWORD" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/commits/${branch}" | jq '.last_pipeline.id')
+
+echo "Triggered CI for branch ${branch}"
+echo "Working with pipeline id #${pipeline_id}"
+echo "Poll timeout set to ${POLL_TIMEOUT}"
+
+ci_status="pending"
+
+until [[ "$ci_status" != "pending" && "$ci_status" != "running" ]]
+do
+   sleep $POLL_TIMEOUT
+   ci_output=$(curl --header "PRIVATE-TOKEN: $GITLAB_PASSWORD" --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/pipelines/${pipeline_id}")
+   ci_status=$(jq -n "$ci_output" | jq -r .status)
+   ci_web_url=$(jq -n "$ci_output" | jq -r .web_url)
+   
+   echo "Current pipeline status: ${ci_status}"
+   if [ "$ci_status" = "running" ]
+   then
+     echo "Checking pipeline status..."
+     curl -d '{"state":"pending", "target_url": "'${ci_web_url}'", "context": "gitlab-ci"}' -H "Authorization: token ${GITHUB_TOKEN}"  -H "Accept: application/vnd.github.antiope-preview+json" -X POST --silent "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}"  > /dev/null 
+   fi
+done
+
+echo "Pipeline finished with status ${ci_status}"
+  
+if [ "$ci_status" = "success" ]
+then 
+  curl -d '{"state":"success", "target_url": "'${ci_web_url}'", "context": "gitlab-ci"}' -H "Authorization: token ${GITHUB_TOKEN}"  -H "Accept: application/vnd.github.antiope-preview+json" -X POST --silent "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}" 
+  exit 0
+elif [[ "$ci_status" = "failed" ]] || [[ "$ci_status" = "null" ]]
+then 
+  curl -d '{"state":"failure", "target_url": "'${ci_web_url}'", "context": "gitlab-ci"}' -H "Authorization: token ${GITHUB_TOKEN}"  -H "Accept: application/vnd.github.antiope-preview+json" -X POST --silent "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}" 
+  exit 1
+fi
